042f268333892fb3e285ac63ae84f14d4cce6af8
sebastianspiegel <sebastian.j.spiegel@gmail.com>
2020-12-01 12:31:39 -0500

    Done.

---
+++ models/piglatinizer.rb
@@ -0,0 +1,37 @@
+class PigLatinizer
+
+    # def piglatinize(text)
+    #     words = text.split(" ")
+    #     words.map do |word|
+    #         w = word.scan /\w/
+    #         first = w.shift
+    #         w.insert(-1, first)
+    #         w << "ay"
+    #         @pigword = w.join.downcase
+    #     end
+    #     @pigword.to_s
+    # end
+
+    def piglatinize(input_str)
+        input_str.split(" ").map{ |word| piglatinize_word(word)}.join(" ")
+      end
+    
+      private
+    
+      def vowel?(char)
+        char.match(/[aAeEiIoOuU]/)
+      end
+    
+      def piglatinize_word(word)
+        # word starts with vowel
+        if vowel?(word[0])
+          word + "way"
+        else
+        # word starts with one or more consonants
+          vowel_index = word.index(/[aAeEiIoOuU]/)
+          consonants = word.slice(0..vowel_index-1)
+          word_leftover = word.slice(vowel_index..word.length)
+          word_leftover + consonants + "ay"
+        end
+      end
+end
